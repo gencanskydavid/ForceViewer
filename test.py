d088362a72e77d1d224b8c3bc7895dae4a26adb4
David Gencansky <dgencansky@alps.cz>
2020-08-16 21:00:04 +0200

    Initial commit.

---
+++ test.py
@@ -0,0 +1,23 @@
+###################################################
+### Only test file
+##################################################
+import os, glob
+import pandas as pd
+import matplotlib.pyplot as plt
+#Load file(s) in folder
+file = glob.glob(os.getcwd()+"\\data\\*.fsl")
+#
+for i in file:
+    fopen = open(i)
+    fopen2 = pd.read_csv(fopen, skiprows=8, header=1)
+    #get name of current file to show in Graph legend
+    name = (i.split('\\'))
+    #add data to graph with correct name
+    plt.plot(fopen2.Fz,label=name[-1])
+#
+plt.ylabel("Force[N]")
+plt.xlabel("Timestamp[1p=3.5ms]")
+plt.title("Timeseries of Force in Z axis")
+plt.legend(loc='upper center', bbox_to_anchor=(1, 0.75))
+plt.show()
+
