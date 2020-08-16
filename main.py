1d3b3de714b695924c2cde51e27373c5bb72b6b1
David Gencansky <dgencansky@alps.cz>
2020-08-16 21:10:51 +0200

    Add Try and Catch exceptions.

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import os, glob
 import pandas as pd
 import matplotlib.pyplot as plt
 #Load file(s) in folder
-file = glob.glob(os.getcwd()+"\\data\\*.fsl")
+try:
+    file = glob.glob(os.getcwd()+"\\data\\*.fsl")
+except Exception as e:
+    print (e)
 #
-for i in file:
-    fopen = open(i)
-    fopen2 = pd.read_csv(fopen, skiprows=8, header=1)
-    #get name of current file to show in Graph legend
-    name = (i.split('\\'))
-    #add data to graph with correct name
-    plt.plot(fopen2.Fz,label=name[-1])
+try:
+    for i in file:
+        fopen = open(i)
+        fopen2 = pd.read_csv(fopen, skiprows=8, header=1)
+        #get name of current file to show in Graph legend
+        name = (i.split('\\'))
+        #add data to graph with correct name
+        plt.plot(fopen2.Fz,label=name[-1])
+except Exception as e:
+    print ("Couldn't open files.",e)
 #
 plt.ylabel("Force[N]")
 plt.xlabel("Timestamp[1p=3.5ms]")
